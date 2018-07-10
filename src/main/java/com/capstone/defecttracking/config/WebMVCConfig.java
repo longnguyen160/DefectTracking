eadb112d80f6764db06b24bc01b78183c0902e69
bimbeonguyen <longnguyen160@gmail.com>
2018-07-10 23:53:07 +0700

    add configuration

--- src/main/java/com/capstone/defecttracking/config/WebMVCConfig.java
+++ src/main/java/com/capstone/defecttracking/config/WebMVCConfig.java
@@ -2,6 +2,7 @@ package com.capstone.defecttracking.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -9,5 +10,14 @@ public class WebMVCConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}")
+            .setViewName("forward:/");
+        registry.addViewController("/**/{spring:\\w+}")
+            .setViewName("forward:/");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+            .setViewName("forward:/");
     }
 }
